fa315557b85a0d4c68c1a80be959cd7e5c2962e2
Jakub Miarka <jakvbmiarka@gmail.com>
2024-08-23 16:47:40 +0200

    added a class for testing results with metaheuristics

---
+++ tomograf/TestFunction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tomograf
+{
+    internal class TestFunction
+    {
+        int n_lasers;
+        double[][] values;
+
+        TestFunction(int n_lasers , double[][] values)
+        {
+            this.n_lasers = n_lasers;
+            this.values = values;
+        }
+
+        double deploy(double[][] args)
+        {
+            double val = 0;
+            for (int i = 0; i < n_lasers; i++) 
+            {
+                for (int j = 0; j < n_lasers; j++)
+                {
+                    val += Math.Abs(args[i][j] - values[i][j]);
+                }
+            }
+            return val;
+        }
+    }
+}
